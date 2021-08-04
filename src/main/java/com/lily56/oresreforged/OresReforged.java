632bcaacf6cf211624d2c61f69683e070f3b323f
Lily56 <85401605+lil56@users.noreply.github.com>
2021-08-04 10:22:04 +0200

    Adjusted fabric.mod.json Added basic onInitialize function

---
+++ src/main/java/com/lily56/oresreforged/OresReforged.java
@@ -0,0 +1,11 @@
+package com.lily56.oresreforged;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OresReforged implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
